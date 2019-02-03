7e42f6aad33fd225bb8816c2e0382fee23f8302a
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2019-02-03 09:40:25 +0000

    add example to run *diag with -Tsvg

--- example/build.sh
+++ example/build.sh
@@ -2,13 +2,19 @@
 
 blockdiag block.diag
 blockdiag -Tpdf block.diag
+blockdiag -Tsvg block.diag
 nwdiag nw.diag
 nwdiag -Tpdf nw.diag
+nwdiag -Tsvg nw.diag
 seqdiag seq.diag
 seqdiag -Tpdf seq.diag
+seqdiag -Tsvg seq.diag
 actdiag act.diag
-actdiag -Tpdf actdiag
+actdiag -Tpdf act.diag
+actdiag -Tsvg act.diag
 rackdiag rack.diag
 rackdiag -Tpdf rack.diag
+rackdiag -Tsvg rack.diag
 packetdiag packet.diag
 packetdiag -Tpdf packet.diag
+packetdiag -Tsvg packet.diag
