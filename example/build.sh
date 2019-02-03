bf66270b81462db6926df9dcc3310310f9c2c344
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2019-02-03 09:51:52 +0000

    add reportlab to support *diag with -Tpdf

--- example/build.sh
+++ example/build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 blockdiag block.diag
-blockdiag -Tpdf block.diag
+blockdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf block.diag
 blockdiag -Tsvg block.diag
 nwdiag nw.diag
-nwdiag -Tpdf nw.diag
+nwdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf nw.diag
 nwdiag -Tsvg nw.diag
 seqdiag seq.diag
-seqdiag -Tpdf seq.diag
+seqdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf seq.diag
 seqdiag -Tsvg seq.diag
 actdiag act.diag
-actdiag -Tpdf act.diag
+actdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf act.diag
 actdiag -Tsvg act.diag
 rackdiag rack.diag
-rackdiag -Tpdf rack.diag
+rackdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf rack.diag
 rackdiag -Tsvg rack.diag
 packetdiag packet.diag
-packetdiag -Tpdf packet.diag
+packetdiag -Tpdf --font=/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf packet.diag
 packetdiag -Tsvg packet.diag
